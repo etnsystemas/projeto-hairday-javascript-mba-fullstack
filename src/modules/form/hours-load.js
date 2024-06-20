a9c24e760b94b1cafa7f4f18ddb183a9bb91a4e6
Einstein Xavier <einstein.xavier@gmail.com>
2024-06-20 17:27:05 -0300

    Renderizando os horários

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -1,6 +1,8 @@
 import dayjs from 'dayjs';
 import {openingHours} from '../../utils/opening-hours';
 
+const hours = document.getElementById('hours')
+
 export function hoursLoad({date}){
   const opening = openingHours.map((hour) => {
     const [scheduleHour] = hour.split(':')
@@ -12,5 +14,13 @@ export function hoursLoad({date}){
     }
   })
 
-  console.log(opening)
+  opening.forEach(({hour, available})=>{
+    const li = document.createElement('li')
+
+    li.classList.add('hour')
+    li.classList.add(available ? 'hour-available' : 'hour-unavailable')
+
+    li.textContent = hour
+    hours.append(li)
+  })
 }
